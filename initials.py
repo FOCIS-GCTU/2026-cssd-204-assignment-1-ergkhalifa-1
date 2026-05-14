82043454bb04172989e85f3b3f3fb8a5c9c5204f
DevLifa <asarealexanobi@gmail.com>
2026-05-14 15:13:46 +0000

    Submit assignment

--- initials.py
+++ initials.py
@@ -2,6 +2,48 @@
 def main():
   pass #code goes here
 
+# File: initials.py
+# Descriptions: The program display the initials "AAA" in small capitals,
+#               with each letter in the output 12 characters wide and 10 characters high
+# Assignment Number: one(1)
+#
+# Name: Alex Asare Anobi
+# STUDENT ID: 2425402074
+# Email: 2425402074@live.gctu.edu.gh
+# Grader: Alex
+#
+# On my honor, Alex Asare nobi, this programming assignment is my own work
+# and I have not provided to any other student.
+
+
+def main():
+    # The program display the initials "AAA" in small capitals,
+    # with each letter in the output 12 characters wide and 10 characters high
+
+    print() # Blank line before small initials
+
+    print ("...AAA") # Small initials (three periods followed by initials with capital letters)
+
+    print("") # Blank line between small and large initials
+
+    print(".......AAAA.................AAAA................AAAA......... ")
+    print("......AAAAAA...............AAAAAA..............AAAAAA........ ")
+    print(".....AA....AA............AA.....AA............AA....AA....... ")
+    print(".....AA....AA............AA.....AA............AA....AA....... ")
+    print("....AAAAAAAAAA..........AAAAAAAAAAA..........AAAAAAAAAA...... ")
+    print("....AAAAAAAAAA..........AAAAAAAAAAA..........AAAAAAAAAA...... ")
+    print("....AA......AA..........AA.......AA..........AA......AA...... ")
+    print("....AA......AA..........AA.......AA..........AA......AA ..... ")
+    print("...AA........AA...**...AA.........AA...**...AA........AA...** ")
+    print("...AA........AA...**...AA.........AA...**...AA........AA...** ")
+
+    print("") # Blank line after the large initials
+
+main()
+
+
+
+
 
 
 main()
